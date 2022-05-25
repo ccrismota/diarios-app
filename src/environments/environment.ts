ac1c87cc46139b180bb04f90f48d9467a891fe21
ccrismota <ccrismota@gmail.com>
2022-05-25 14:13:42 -0300

    first commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'diario-viagens-a160f',
+    appId: '1:592237195575:web:3382c03e7666722aa02ab2',
+    storageBucket: 'diario-viagens-a160f.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyDGO1JSVrHwZosZ7S6HuV6Yd76FRmvp1cs',
+    authDomain: 'diario-viagens-a160f.firebaseapp.com',
+    messagingSenderId: '592237195575',
+  },
   production: false
 };
 
